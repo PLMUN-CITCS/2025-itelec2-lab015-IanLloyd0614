4a8d5de2052d5c9d01ebe29e715f0138fadf7340
unknown <angelesianlloyd_bist@gmail.com>
2025-02-19 22:44:42 +0800

    Submitting Python Week 04 - Laboratory # 15

---
+++ while_loop_sentinel.py
@@ -0,0 +1,22 @@
+# Ian Lloyd Santiago Angeles
+# ITELEC2
+# Laboratory #15 – Guided Coding Exercise: While Loop for User Input with a Sentinel Value
+
+def main():
+   total_sum = 0
+
+   while True:
+        user_input = input("Enter a number (or 'stop' to finish): ")
+        if user_input.strip().lower() == "stop":
+            break 
+            
+        try:
+            number = float(user_input)
+            total_sum += number
+        except ValueError:
+            print("Invalid input. Please enter a numeric value or 'stop'.")
+
+    print("The total sum is:", total_sum)
+
+if __name__ == "__main__":
+    main()
